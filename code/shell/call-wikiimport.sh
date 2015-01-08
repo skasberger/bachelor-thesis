60442f11366e972aad5e20615809412d45c5bbec
Stefan Kasberger <mail@stefankasberger.at>
2015-01-09 00:03:12 +0100

    add shell scripts which call python scripts

---
+++ code/shell/call-wikiimport.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+cd ../python
+python wikiimport.py -t Goranci -o '/opendada/science/academia/bakk/tmp'
+
+python wikiimport.py -t 45th_Infantry_Division_(United_States) -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Alden_Partridge -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Battle_of_Chickamauga -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Mary_Martha_Sherwood -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Welding -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t William_McKendree -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t SMS_Von_der_Tann -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t List_of_The_Penguins_of_Madagascar_episodes -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Blue_iguana -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Beaumont-Hamel_Newfoundland_Memorial -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Blank_Page -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Jake_Gyllenhaal_filmography -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t List_of_colossal_squid_specimens_and_sightings -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t List_of_record_labels:_I–Q -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t List_of_vegetable_oils -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Territorial_era_of_Minnesota -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t The_Whitlams -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t Write_Me_Back -o '/opendada/science/academia/bakk/tmp'
+python wikiimport.py -t You_Are_the_Apple_of_My_Eye -o '/opendada/science/academia/bakk/tmp'
+
+
+
---
+++ code/shell/call-wikistats.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+cd ../python
+python wikistats.py -i '../../data/raw/xml/Finger_Lakes3.xml' 
+
+python wikistats.py -i '../../data/raw/xml/se-paper/45th_Infantry_Division_(United_States).xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Goranci.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Alden_Partridge.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Battle_of_Chickamauga.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Mary_Martha_Sherwood.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Welding.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/William_McKendree.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Margaret_Kilgallen.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/SMS_Von_der_Tann.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/List_of_The_Penguins_of_Madagascar_episodes.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Blue_iguana.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Beaumont-Hamel_Newfoundland_Memorial.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Blank_Page.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Jake_Gyllenhaal_filmography.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/List_of_colossal_squid_specimens_and_sightings.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/List_of_record_labels:_I–Q.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/List_of_vegetable_oils.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Territorial_era_of_Minnesota.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/The_Whitlams.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/Write_Me_Back.xml' 
+python wikistats.py -i '../../data/raw/xml/se-paper/You_Are_the_Apple_of_My_Eye.xml' 
+
+
+
+
+
+
